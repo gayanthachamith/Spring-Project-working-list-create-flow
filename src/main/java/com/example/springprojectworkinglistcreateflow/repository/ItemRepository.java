992e6e2690dea1b5f03a38d96cae96d9def9feed
Kakha8 <kakha.kudava.98@gmail.com>
2026-02-11 11:44:05 +0200

    added repo

---
+++ src/main/java/com/example/springprojectworkinglistcreateflow/repository/ItemRepository.java
@@ -0,0 +1,11 @@
+package com.example.springprojectworkinglistcreateflow.repository;
+
+import com.example.springprojectworkinglistcreateflow.entity.Items;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Items, Long> {
+    Long findIdByName(String name);
+    List<Items> findAll(String name);
+}
