d89211206932c02d30103acedee8dd9d552eb03f
Kakha8 <kakha.kudava.98@gmail.com>
2026-02-11 11:22:30 +0200

    created entity

---
+++ src/main/java/com/example/springprojectworkinglistcreateflow/entity/Items.java
@@ -0,0 +1,36 @@
+package com.example.springprojectworkinglistcreateflow.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Items {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+
+}
